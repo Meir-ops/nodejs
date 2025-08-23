2b2a7a2756d9aeb2fcaf6bab46647db2c339fb39
Meir Plevinski <imagestore34@gmail.com>
2025-08-23 22:49:21 +0300

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+   <>
+    <div className="h-screen flex items-center justify-center   overflow-y-scroll hide-scrollbar">
+      <div className="text-center">
+        <h3 className="text-pink-700 text-2xl font-bold">
+          <strong>intibergs.com</strong>
+        </h3>
+        <p className="text-gray-700 text-2xl">
+          Your comfort, our priority.
+        </p>
+        <h2 className="text-black text-3xl font-semibold">
+          Discover Premium Lingerie Designed for Confidence & Comfort
+        </h2>
+        <p className="text-gray-600 text-lg">
+          Shop bras, panties, shapewear, and more — expertly crafted to fit your
+          lifestyle. Join thousands of women who trust Bergs for everyday
+          elegance and support.
+        </p>
+      </div>
+    </div>
+   </>
+  );
+}
